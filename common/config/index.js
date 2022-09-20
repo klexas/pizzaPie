c0d4216b97cc949598916e48c44532af89f459f8
Adam Robins <iam@adamrobins.com>
2022-09-21 01:49:19 +0200

    node can be microservice - dependacy based too - It's slow as hell but it works

---
+++ common/config/index.js
@@ -0,0 +1,7 @@
+// TODO: Move to an .env file
+module.exports = {
+    PORT: 3000,
+    MAX_PROCESS: 2,
+    MAX_REQUEST: 100,
+    MAX_REQUEST_PER_MINUTE: 100
+};
---
+++ common/constants/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    GREEATING: 'This is the hubspot or - gateway manager',
+    LOAD_SUCCESS: 'Service loaded successfully',
+    LOAD_FAIL: 'Service failed to load'
+};
